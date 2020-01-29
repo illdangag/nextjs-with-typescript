0917028993633d3b6ec3e6372d7b84426950684a
illdangag <illdangag@gmail.com>
2020-01-30 01:34:26 +0900

    with redux thunk

---
+++ pages/_app.tsx
@@ -0,0 +1,21 @@
+import App from 'next/app';
+import React from 'react';
+import withReduxStore from '../utils/with-redux-store';
+import { Provider, } from 'react-redux';
+
+type Props = {
+  reduxStore: any;
+};
+
+class MyApp extends App<Props> {
+  render () {
+    const { Component, pageProps, reduxStore, } = this.props;
+    return (
+      <Provider store={reduxStore}>
+        <Component {...pageProps} />
+      </Provider>
+    );
+  }
+}
+
+export default withReduxStore(MyApp);
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,28 @@
-import * as React from 'react';
+import React, { useEffect, } from 'react';
 import Link from 'next/link';
 import Layout from '../components/layout';
 import { NextPage, } from 'next';
+import Clock from '../components/clock';
 
-const IndexPage: NextPage = () => {
+import { connect, useDispatch, } from 'react-redux';
+import { Dispatchable, mapDispatchToProps, } from '../utils/with-redux-store';
+import { startClock, } from '../store';
+
+type Props = {};
+
+const IndexPage: NextPage<Dispatchable<Props>> = () => {
+  const dispatch = useDispatch();
+
+  let timer: any = null;
+
+  useEffect(() => {
+    if (timer !== null) {
+      clearInterval(timer);
+    }
+
+    timer = startClock(dispatch);
+  });
+
   return (
     <Layout title='Home | Next.js + TypeScript Example'>
       <h1>Hello Next.js 👋</h1>
@@ -12,8 +31,9 @@ const IndexPage: NextPage = () => {
           <a>About</a>
         </Link>
       </p>
+      <Clock/>
     </Layout>
   );
 };
 
-export default IndexPage;
+export default connect(null, mapDispatchToProps)(IndexPage);
